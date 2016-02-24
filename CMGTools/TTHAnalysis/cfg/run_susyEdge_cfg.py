f4769fe2fe363d703afef19a8062fcf9bc97b118
mdunser <marc.dunser@cern.ch>
2016-02-24 11:18:59 +0100

    slowly moving to 76x. preparing stuff for sergio

--- CMGTools/TTHAnalysis/cfg/run_susyEdge_cfg.py
+++ CMGTools/TTHAnalysis/cfg/run_susyEdge_cfg.py
@@ -34,6 +34,7 @@ ttHLepSkim.maxLeptons = 999
 #ttHLepSkim.idCut  = ""
 #ttHLepSkim.ptCuts = []
 
+runSMS = False
 
 ##########################################################
 ##################Lepton Analyzier########################
@@ -42,7 +43,8 @@ lepAna.doMiniIsolation = True
 lepAna.packedCandidates = 'packedPFCandidates'
 lepAna.miniIsolationPUCorr = 'rhoArea'
 lepAna.miniIsolationVetoLeptons = None # use 'inclusive' to veto inclusive leptons and their footprint in all isolation cones
-lepAna.loose_electron_id  = "POG_MVA_ID_Phys14_NonTrig_VLoose"
+## this is new lepAna.loose_electron_id  = "POG_MVA_ID_Phys14_NonTrig_VLoose"
+lepAna.loose_electron_id = "POG_MVA_ID_Spring15_NonTrig_VLooseIdEmu"
 lepAna.loose_muon_id      = "POG_ID_Loose"
 lepAna.loose_electron_eta = 2.5
 lepAna.loose_electron_pt  = 15
@@ -131,6 +133,17 @@ ttHJetTauAna = cfg.Analyzer(
 
 
 ##########################################################
+############Insertion in susyCoreSequence#################
+##########################################################
+## Insert the FatJet, SV, HeavyFlavour analyzers in the sequence
+susyCoreSequence.insert(susyCoreSequence.index(ttHCoreEventAna), 
+                        ttHFatJetAna)
+susyCoreSequence.insert(susyCoreSequence.index(ttHCoreEventAna), 
+                        ttHSVAna)
+susyCoreSequence.insert(susyCoreSequence.index(ttHCoreEventAna), 
+                        ttHHeavyFlavourHadronAna)
+
+##########################################################
 #################DeClustering Analyzer####################
 ##########################################################
 ## Insert declustering analyzer
@@ -155,6 +168,7 @@ ttHDecluster = cfg.Analyzer(
     mcTauPtCut    = 15,
     )
 
+susyCoreSequence.insert(susyCoreSequence.index(ttHFatJetAna)+1, ttHDecluster)
 
 ##########################################################
 #############TreeProducer SusyMultilepton#################
@@ -174,32 +188,24 @@ treeProducer = cfg.Analyzer(
 
 
 
-##########################################################
-############Insertion in susyCoreSequence#################
-##########################################################
-## Insert the FatJet, SV, HeavyFlavour analyzers in the sequence
-susyCoreSequence.insert(susyCoreSequence.index(ttHCoreEventAna), 
-                        ttHFatJetAna)
-susyCoreSequence.insert(susyCoreSequence.index(ttHCoreEventAna), 
-                        ttHSVAna)
-susyCoreSequence.insert(susyCoreSequence.index(ttHCoreEventAna), 
-                        ttHHeavyFlavourHadronAna)
-
-susyCoreSequence.insert(susyCoreSequence.index(ttHFatJetAna)+1, ttHDecluster)
 
 ## histo counter
-susyCoreSequence.insert(susyCoreSequence.index(skimAnalyzer),
-                        susyCounter)
+if not runSMS:
+    susyCoreSequence.insert(susyCoreSequence.index(skimAnalyzer),
+                            susyCounter)
+else:
+    susyCoreSequence.insert(susyCoreSequence.index(susyScanAna)+1,susyCounter)
 
 # HBHE new filter
 from CMGTools.TTHAnalysis.analyzers.hbheAnalyzer import hbheAnalyzer
 hbheAna = cfg.Analyzer(
     hbheAnalyzer, name="hbheAnalyzer", IgnoreTS4TS5ifJetInLowBVRegion=False
     )
-susyCoreSequence.insert(susyCoreSequence.index(ttHCoreEventAna),hbheAna)
-treeProducer.globalVariables.append(NTupleVariable("hbheFilterNew50ns", lambda ev: ev.hbheFilterNew50ns, int, help="new HBHE filter for 50 ns"))
-treeProducer.globalVariables.append(NTupleVariable("hbheFilterNew25ns", lambda ev: ev.hbheFilterNew25ns, int, help="new HBHE filter for 25 ns"))
-treeProducer.globalVariables.append(NTupleVariable("hbheFilterIso", lambda ev: ev.hbheFilterIso, int, help="HBHE iso-based noise filter"))
+if not runSMS:
+    susyCoreSequence.insert(susyCoreSequence.index(ttHCoreEventAna),hbheAna)
+    treeProducer.globalVariables.append(NTupleVariable("hbheFilterNew50ns", lambda ev: ev.hbheFilterNew50ns, int, help="new HBHE filter for 50 ns"))
+    treeProducer.globalVariables.append(NTupleVariable("hbheFilterNew25ns", lambda ev: ev.hbheFilterNew25ns, int, help="new HBHE filter for 25 ns"))
+    treeProducer.globalVariables.append(NTupleVariable("hbheFilterIso", lambda ev: ev.hbheFilterIso, int, help="HBHE iso-based noise filter"))
 
 
 ## not needed for Oct05 and prompt-v4doT1METCorr = True
@@ -233,6 +239,7 @@ triggerFlagsAna.triggerBits = {
     'pfht400' : triggers_pfht400,
     'pfht475' : triggers_HT475,
     'pfht600' : triggers_HT600,
+    'pfht650' : triggers_pfht650,
     'pfht800' : triggers_HT800,
     'pfht900' : triggers_HT900,
     'at57' : triggers_at57,
@@ -253,12 +260,18 @@ triggerFlagsAna.triggerBits = {
     'HTJet' : triggers_htjet,
 }
 
+if runSMS:
+    susyCoreSequence.remove(triggerFlagsAna)
+    susyCoreSequence.remove(triggerAna)
+    susyCoreSequence.remove(eventFlagsAna)
+
 
 ##########################################################
 ################### Sample imports  ######################
 ##########################################################
 from CMGTools.RootTools.samples.samples_13TeV_RunIISpring15MiniAODv2 import *
 from CMGTools.RootTools.samples.samples_13TeV_DATA2015 import *
+from CMGTools.RootTools.samples.samples_13TeV_signals import *
 
 
 ##########################################################
@@ -291,7 +304,9 @@ if runData:
     ## json = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-258750_13TeV_PromptReco_Collisions15_25ns_JSON.txt"
 
     ## this is the JSON for the full dataset of the year 2015. 2.11 inverse femtobarns
-    json = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON.txt"
+    #json = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON.txt"
+    ## this is the JSON for moriond
+    json = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_v2.txt"
 
     ## # Run2015C, 25 ns, 3.8T
     ## processing = "Run2015C-05Oct2015-v1"; short = "Run2015C_miniAODv2"; run_ranges = [ (246908,260627) ]; useAAA=False; is50ns=False; triggerFlagsAna.checkL1Prescale = False;
@@ -312,11 +327,11 @@ if runData:
     DatasetsAndTriggers = []
     selectedComponents = []; vetos = []  
 
-    useAAA = False
+    useAAA = True
     is50ns = False
     processings = ["Run2015C_25ns-05Oct2015-v1", "Run2015D-05Oct2015-v1", "Run2015D-PromptReco-v4"]
     shorts      = ["Run2015C_25ns-05Oct_v1"    , "Run2015D-05Oct_v1"    , "Run2015D_v4"]
-    run_ranges  = [ (246908,260627) ]
+    run_ranges  = [ (246908,260628) ]
  
     ## hadtriggers = triggers_pfht200 + triggers_pfht250 + triggers_pfht300 + triggers_ht350 + triggers_pfht400 + triggers_ht475 + triggers_ht600 + triggers_HT800 + triggers_HT900 + triggers_at57 + triggers_at55 + triggers_at53 + triggers_at52 + triggers_at51
     ## DatasetsAndTriggers.append( ("DoubleMuon", triggers_mu17mu8 + triggers_mu17mu8_dz + triggers_mu17tkmu8_dz + triggers_mumu_noniso_50ns) )
@@ -359,25 +374,42 @@ if runData:
                                                  useAAA=useAAA)
                 print "Will process %s (%d files)" % (comp.name, len(comp.files))
 #                print "\ttrigger sel %s, veto %s" % (triggers, vetos)
-                comp.splitFactor = len(comp.files)/3
+                comp.splitFactor = len(comp.files)/5
                 comp.fineSplitFactor = 1
                 selectedComponents.append( comp )
             #vetos += triggers
     if json is None:
         susyCoreSequence.remove(jsonAna)
 
+if runSMS:
+    jetAna.mcGT = "MCRUN2_74_V9_FASTSIM_291115"
+    jetAnaScaleUp.mcGT = "MCRUN2_74_V9_FASTSIM_291115"
+    jetAnaScaleDown.mcGT = "MCRUN2_74_V9_FASTSIM_291115"
+    jetAna.applyL2L3Residual = False
+    jetAnaScaleUp.applyL2L3Residual = False
+    jetAnaScaleDown.applyL2L3Residual = False
+    useAAA = False ## accesses only files on EOS or at CERN
+
+    print 'I\'m in the sms test thing here!!'
+    selectedComponents = [SMS_T6bbllslepton_mSbottom_400To550_mLSP_200To500_miniAODv2]#, SMS_T6bbllslepton_mSbottom_600To900_mLSP_200To800_miniAODv2]
+    comp.files = comp.files[:1]
+    #for comp in selectedComponents:
+    #    comp.splitFactor = 500
+    #    #comp.finesplitFactor = 4
+    ##comp.splitFactor = 500
+    ##comp.fineSplitFactor = 4
 
 from PhysicsTools.HeppyCore.framework.heppy_loop import getHeppyOption
 test = getHeppyOption('test')
 #test = '74X-MC'
 if test == 'synch':
     print 'I\'m in the synch test thing here!!'
-    comp = TTLep_pow
+    #comp = TTLep_pow
+    #selectedComponents = [comp]
+    comp = selectedComponents[0]
     selectedComponents = [comp]
     #comp.files = comp.files[:1]
-    comp.files = [
-    '/afs/cern.ch/work/m/mdunser/public/synchFiles/004613BA-C46D-E511-9EB6-001E67248732.root',
-    ]
+    comp.files = comp.files[:1]#[ '/afs/cern.ch/work/m/mdunser/public/synchFiles/004613BA-C46D-E511-9EB6-001E67248732.root' ]
     #comp.finesplitFactor = 10
     #comp.finesplitFactor = 4
 elif test == '74X-MC':
@@ -403,38 +435,6 @@ elif test == '74X-MC':
             comp.files = comp.files[:1]
             comp.splitFactor = 1
             comp.fineSplitFactor = 1 if getHeppyOption("single") else 4
-elif test == '74X-Data':
-    #selectedComponents = [DoubleMuon_Run2015B, MuonEG_Run2015B, DoubleEG_Run2015B, JetHT_Run2015B, HTMHT_Run2015B]
-    selectedComponents = [JetHT_Run2015B]#, HTMHT_Run2015B]
-    #selectedComponents = [DoubleMuon_Run2015B]
-    eventFlagsAna.processName = 'HLT'
-    jetAna.recalibrateJets = False
-    jetAna.smearJets       = False 
-    photonAna.do_mc_match = False
-    vertexAna.keepFailingEvents = True 
-    jsonAna.useLumiBlocks = True
-    #sequence.remove(jsonAna)
-    for comp in selectedComponents:
-        comp.isMC = False
-        comp.isData = True
-        #comp.files = ["/afs/cern.ch/work/p/pablom/public/E42FEF61-6E27-E511-B93A-02163E0143C0.root"]
-        comp.files = comp.files[:1]
-
-elif test == "express":
-    selectedComponents = [ MuEG_740p9 ]
-    comp = selectedComponents[0]
-    comp.files = [ 'root://eoscms//eos/cms/store/express/Run2015A/ExpressPhysics/FEVT/Express-v1/000/246/908/00000/04B152E7-DE09-E511-8B18-02163E011D4A.root' ]
-    comp.name  = 'ExpressPhysics'
-    comp.triggers = []
-    comp.json     = None
-    jetAna.recalibrateJets = False 
-    ttHLepSkim.minLeptons = 0
-    # preprocessor cfg to be created with
-    #    cmsDriver.py miniAOD-data -s PAT --data --runUnscheduled --eventcontent MINIAOD --conditions GR_P_V56 --no_exec
-    #    sed -i 's/process.MINIAODoutput_step/process.endpath/' miniAOD-data_PAT.py
-    from PhysicsTools.Heppy.utils.cmsswPreprocessor import CmsswPreprocessor
-    preprocessor = CmsswPreprocessor("miniAOD-data_PAT.py")
-
 
 
 ##########################################################
@@ -458,6 +458,7 @@ outputService.append(output_service)
 from PhysicsTools.HeppyCore.framework.eventsfwlite import Events
 from CMGTools.TTHAnalysis.tools.EOSEventsWithDownload import EOSEventsWithDownload
 event_class = EOSEventsWithDownload
+EOSEventsWithDownload.aggressive = 2 # always fetch if running on Wigner
 
 if getHeppyOption("nofetch"):
     event_class = Events 
--- CMGTools/TTHAnalysis/python/tools/edgeFriends.py
+++ CMGTools/TTHAnalysis/python/tools/edgeFriends.py
@@ -1,4 +1,16 @@
+## if MT2 doesnt work, put this line in the MT2 file: from ROOT.heppy import Davismt2
+
 from CMGTools.TTHAnalysis.treeReAnalyzer import *
+from CMGTools.TTHAnalysis.tools.eventVars_MT2 import *
+print 'i am loading that stuff'
+from PhysicsTools.Heppy.analyzers.core.AutoHandle import AutoHandle
+ROOT.gSystem.Load("libFWCoreFWLite.so")
+ROOT.gSystem.Load("libDataFormatsFWLite.so")
+ROOT.AutoLibraryLoader.enable()
+print 'done loading stuff. trying to include davismt2'
+
+from ROOT.heppy import Davismt2
+#from ROOT import Davismt2
 import copy
 
 class edgeFriends:
@@ -19,18 +31,28 @@ class edgeFriends:
             for i in list(self.beamHaloListFile):
                 self.beamHaloSet.add(i.rstrip('\n'))
         self.beamHaloListFile.close()
-        self.lh_file = ROOT.TFile('/afs/cern.ch/work/m/mdunser/public/pdfsForLikelihood/pdfs_version6.root')
-        self.h_lh_summlb_data = copy.deepcopy(self.lh_file.Get('emu_data_pdf_histo_mlb_ds_cuts_of_sr_met150'))
-        self.h_lh_lepsdr_data = copy.deepcopy(self.lh_file.Get('emu_data_pdf_histo_ldr_ds_cuts_of_sr_met150'))
-        self.h_lh_met_data    = copy.deepcopy(self.lh_file.Get('emu_data_pdf_histo_met_ds_cuts_of_sr_met150'))
-        self.h_lh_zpt_data    = copy.deepcopy(self.lh_file.Get('emu_data_pdf_histo_zpt_ds_cuts_of_sr_met150'))
-        self.h_lh_st_data     = copy.deepcopy(self.lh_file.Get('emu_data_pdf_histo_st_ds_cuts_of_sr_met150' ))
-        self.h_lh_summlb_mc   = copy.deepcopy(self.lh_file.Get('tt_mc_pdf_histo_mlb_ds_cuts_of_sr_met150'   ))
-        self.h_lh_lepsdr_mc   = copy.deepcopy(self.lh_file.Get('tt_mc_pdf_histo_ldr_ds_cuts_of_sr_met150'   ))
-        self.h_lh_met_mc      = copy.deepcopy(self.lh_file.Get('tt_mc_pdf_histo_met_ds_cuts_of_sr_met150'   ))
-        self.h_lh_zpt_mc      = copy.deepcopy(self.lh_file.Get('tt_mc_pdf_histo_zpt_ds_cuts_of_sr_met150'   ))
-        self.h_lh_st_mc       = copy.deepcopy(self.lh_file.Get('tt_mc_pdf_histo_st_ds_cuts_of_sr_met150'    ))
+        ## load the pdf file for the likelihood
+        self.lh_file = ROOT.TFile('/afs/cern.ch/work/m/mdunser/public/pdfsForLikelihood/pdfs_version11.root')
+        # these are the kernel pdfs
+        self.h_lh_zpt_data = copy.deepcopy(self.lh_file.Get('em_data_pdf_histo_zpt_ds_cuts_of_sr_met150')); self.h_lh_zpt_data.Scale(1./self.h_lh_zpt_data.Integral())
+        self.h_lh_met_data = copy.deepcopy(self.lh_file.Get('em_data_pdf_histo_met_ds_cuts_of_sr_met150')); self.h_lh_met_data.Scale(1./self.h_lh_met_data.Integral())
+        self.h_lh_mlb_data = copy.deepcopy(self.lh_file.Get('em_data_pdf_histo_mlb_ds_cuts_of_sr_met150')); self.h_lh_mlb_data.Scale(1./self.h_lh_mlb_data.Integral())
+        self.h_lh_ldr_data = copy.deepcopy(self.lh_file.Get('em_data_pdf_histo_ldr_ds_cuts_of_sr_met150')); self.h_lh_ldr_data.Scale(1./self.h_lh_ldr_data.Integral())
+        self.h_lh_zpt_mc   = copy.deepcopy(self.lh_file.Get('tt_mc_pdf_histo_zpt_ds_cuts_of_sr_met150'  )); self.h_lh_zpt_mc  .Scale(1./self.h_lh_zpt_mc  .Integral())
+        self.h_lh_met_mc   = copy.deepcopy(self.lh_file.Get('tt_mc_pdf_histo_met_ds_cuts_of_sr_met150'  )); self.h_lh_met_mc  .Scale(1./self.h_lh_met_mc  .Integral())
+        self.h_lh_mlb_mc   = copy.deepcopy(self.lh_file.Get('tt_mc_pdf_histo_mlb_ds_cuts_of_sr_met150'  )); self.h_lh_mlb_mc  .Scale(1./self.h_lh_mlb_mc  .Integral())
+        self.h_lh_ldr_mc   = copy.deepcopy(self.lh_file.Get('tt_mc_pdf_histo_ldr_ds_cuts_of_sr_met150'  )); self.h_lh_ldr_mc  .Scale(1./self.h_lh_ldr_mc  .Integral())
+        # these are the analytical pdfs
+        self.h_lh_ana_zpt_data = copy.deepcopy(self.lh_file.Get('em_data_fit_histo_zpt_ds_cuts_of_sr_met150__lepsZPt_Edge')); self.h_lh_ana_zpt_data.Scale(1./self.h_lh_ana_zpt_data.Integral())
+        self.h_lh_ana_met_data = copy.deepcopy(self.lh_file.Get('em_data_fit_histo_met_ds_cuts_of_sr_met150__met_pt'      )); self.h_lh_ana_met_data.Scale(1./self.h_lh_ana_met_data.Integral())
+        self.h_lh_ana_mlb_data = copy.deepcopy(self.lh_file.Get('em_data_fit_histo_mlb_ds_cuts_of_sr_met150__sum_mlb_Edge')); self.h_lh_ana_mlb_data.Scale(1./self.h_lh_ana_mlb_data.Integral())
+        self.h_lh_ana_ldr_data = copy.deepcopy(self.lh_file.Get('em_data_fit_histo_ldr_ds_cuts_of_sr_met150__lepsDR_Edge' )); self.h_lh_ana_ldr_data.Scale(1./self.h_lh_ana_ldr_data.Integral())
+        self.h_lh_ana_zpt_mc   = copy.deepcopy(self.lh_file.Get('tt_mc_fit_histo_zpt_ds_cuts_of_sr_met150__lepsZPt_Edge'  )); self.h_lh_ana_zpt_mc  .Scale(1./self.h_lh_ana_zpt_mc  .Integral())
+        self.h_lh_ana_met_mc   = copy.deepcopy(self.lh_file.Get('tt_mc_fit_histo_met_ds_cuts_of_sr_met150__met_pt'        )); self.h_lh_ana_met_mc  .Scale(1./self.h_lh_ana_met_mc  .Integral())
+        self.h_lh_ana_mlb_mc   = copy.deepcopy(self.lh_file.Get('tt_mc_fit_histo_mlb_ds_cuts_of_sr_met150__sum_mlb_Edge'  )); self.h_lh_ana_mlb_mc  .Scale(1./self.h_lh_ana_mlb_mc  .Integral())
+        self.h_lh_ana_ldr_mc   = copy.deepcopy(self.lh_file.Get('tt_mc_fit_histo_ldr_ds_cuts_of_sr_met150__lepsDR_Edge'   )); self.h_lh_ana_ldr_mc  .Scale(1./self.h_lh_ana_ldr_mc  .Integral())
         self.lh_file.Close()
+
     def listBranches(self):
         label = self.label
         biglist = [ ("nLepTight"+label, "I"), ("nJetSel"+label, "I"), ("nPairLep"+label, "I"),
@@ -42,9 +64,16 @@ class edgeFriends:
                  ("lepsMll"+label, "F"), ("lepsJZB"+label, "F"), ("lepsDR"+label, "F"), ("lepsMETRec"+label, "F"), ("lepsZPt"+label, "F"), ("metl1DPhi"+label, "F"), ("metl2DPhi"+label, "F"), ("lepsDPhi"+label, "F"),
                  ("Lep1_pt"+label, "F"), ("Lep1_eta"+label, "F"), ("Lep1_phi"+label, "F"), ("Lep1_miniRelIso"+label, "F"), ("Lep1_pdgId"+label, "I"), ("Lep1_mvaIdSpring15"+label, "F"), ("Lep1_minTauDR"+label, "F"),
                  ("Lep2_pt"+label, "F"), ("Lep2_eta"+label, "F"), ("Lep2_phi"+label, "F"), ("Lep2_miniRelIso"+label, "F"), ("Lep2_pdgId"+label, "I"), ("Lep2_mvaIdSpring15"+label, "F"), ("Lep2_minTauDR"+label, "F"),
-                 ("PileupW"+label, "F"), ("min_mlb1"+label, "F"), ("min_mlb2"+label, "F"), ("sum_mlb"+label, "F"), ("st"+label,"F"), ("srID"+label, "I"), 
-                 ("lh_met_data"+label, "F") , ("lh_zpt_data"+label, "F") , ("lh_summlb_data"+label, "F") , ("lh_lepsdr_data"+label, "F") , ("lh_st_data"+label, "F"),
-                 ("lh_met_mc"+label  , "F") , ("lh_zpt_mc"+label  , "F") , ("lh_summlb_mc"+label  , "F") , ("lh_lepsdr_mc"+label  , "F") , ("lh_st_mc"+label  , "F")
+                 ("PileupW"+label, "F"), ("min_mlb1"+label, "F"), ("min_mlb2"+label, "F"), ("sum_mlb"+label, "F"), ("st"+label,"F"), ("srID"+label, "I"), ("mt2"+label, "F"),
+                 ("lh_zpt_data"+label, "F") , ("lh_met_data"+label, "F") , ("lh_mlb_data"+label, "F") , ("lh_ldr_data"+label, "F") ,
+                 ("lh_zpt_mc"+label  , "F") , ("lh_met_mc"+label  , "F") , ("lh_mlb_mc"+label  , "F") , ("lh_ldr_mc"+label  , "F") ,
+                 ("lh_ana_zpt_data"+label, "F") , ("lh_ana_met_data"+label, "F") , ("lh_ana_mlb_data"+label, "F") , ("lh_ana_ldr_data"+label, "F") ,
+                 ("lh_ana_zpt_mc"+label  , "F") , ("lh_ana_met_mc"+label  , "F") , ("lh_ana_mlb_mc"+label  , "F") , ("lh_ana_ldr_mc"+label  , "F") ,
+
+                 ("cum_zpt_data"+label, "F") , ("cum_met_data"+label, "F") , ("cum_mlb_data"+label, "F") , ("cum_ldr_data"+label, "F") ,
+                 ("cum_zpt_mc"+label  , "F") , ("cum_met_mc"+label  , "F") , ("cum_mlb_mc"+label  , "F") , ("cum_ldr_mc"+label  , "F") ,
+                 ("cum_ana_zpt_data"+label, "F") , ("cum_ana_met_data"+label, "F") , ("cum_ana_mlb_data"+label, "F") , ("cum_ana_ldr_data"+label, "F") ,
+                 ("cum_ana_zpt_mc"+label  , "F") , ("cum_ana_met_mc"+label  , "F") , ("cum_ana_mlb_mc"+label  , "F") , ("cum_ana_ldr_mc"+label  , "F")
                  
                  ]
         ## for lfloat in 'pt eta phi miniRelIso pdgId'.split():
@@ -64,6 +93,7 @@ class edgeFriends:
         lepso = [l for l in Collection(event,"LepOther","nLepOther")]
         jetsc = [j for j in Collection(event,"Jet","nJet")]
         jetsd = [j for j in Collection(event,"DiscJet","nDiscJet")]
+        metco = [m for m in Collection(event,"metcJet","nDiscJet")]
         (met, metphi)  = event.met_pt, event.met_phi
         if self.isMC:
             gentaus  = [t for t in Collection(event,"genTau","ngenTau")]
@@ -129,6 +159,7 @@ class edgeFriends:
         l1 = ROOT.TLorentzVector()
         l2 = ROOT.TLorentzVector()
         ltlvs = [l1, l2]
+        lepvectors = []
 
         for lfloat in 'pt eta phi miniRelIso pdgId mvaIdSpring15'.split():
             if lfloat == 'pdgId':
@@ -139,16 +170,10 @@ class edgeFriends:
                 lepret["Lep2_"+lfloat+self.label] = -42.
         if ret['iL1T'] != -999 and ret['iL2T'] != -999:
             ret['nPairLep'] = 2
-#            print 'index of lepton 1 %d index of lepton 2 %d' %( ret['iL1T'], ret['iL2T'])
-            ## for lfloat in 'pt eta phi miniRelIso pdgId'.split():
-            ##     lepret["Lep1_"+lfloat+label] = -42.
-            ##     lepret["Lep2_"+lfloat+label] = -42.
             # compute the variables for the two leptons in the pair
             lcount = 1
             for idx in [ret['iL1T'], ret['iL2T']]:
                 lep = leps[idx] if idx >= 0 else lepso[-1-idx]
-                #for lfloat in 'pt eta phi miniRelIso pdgId'.split():
-                #    lepret[lfloat].append( getattr(lep,lfloat) )
                 minDRTau = 99.
                 if self.isMC:
                     for tau in gentaus:
@@ -157,6 +182,7 @@ class edgeFriends:
                             minDRTau = tmp_dr
                 for lfloat in 'pt eta phi miniRelIso pdgId mvaIdSpring15'.split():
                     lepret["Lep"+str(lcount)+"_"+lfloat+self.label] = getattr(lep,lfloat)
+                lepvectors.append(lep)
                 lepret['metl'+str(lcount)+'DPhi'+self.label] = abs( deltaPhi( getattr(lep, 'phi'), metphi ))
                 lepret["Lep"+str(lcount)+"_"+"minTauDR"+self.label] = minDRTau
                 ltlvs[lcount-1].SetPtEtaPhiM(lep.pt, lep.eta, lep.phi, 0.0005 if lep.pdgId == 11 else 0.106)
@@ -164,6 +190,14 @@ class edgeFriends:
                 #print 'good lepton', getattr(lep,'pt'), getattr(lep,'eta'), getattr(lep,'phi'), getattr(lep,'pdgId') 
         else:
             ret['nPairLep'] = 0
+
+        mt2 = -1.
+        if ret['nPairLep'] == 2:
+            l1mt2 = ROOT.reco.Particle.LorentzVector(lepvectors[0].p4().Px(), lepvectors[0].p4().Py(),lepvectors[0].p4().Pz(),lepvectors[0].p4().Energy())
+            l2mt2 = ROOT.reco.Particle.LorentzVector(lepvectors[1].p4().Px(), lepvectors[1].p4().Py(),lepvectors[1].p4().Pz(),lepvectors[1].p4().Energy())
+            metp4obj = ROOT.reco.Particle.LorentzVector(met*cos(metphi),met*sin(metphi),0,met)
+            mt2 = computeMT2(l1mt2, l2mt2, metp4obj)
+        ret['mt2'] = mt2
             
         ### Define jets
         ret["iJ"] = []
@@ -283,25 +317,34 @@ class edgeFriends:
             srID = self.getSRID(ret['lepsMll'], lepret["Lep1_eta"+self.label], lepret["Lep2_eta"+self.label], ret["nBJetMedium35"])
             ret["srID"] = srID
             for t in ['data', 'mc']:
-                ret["lh_summlb_%s"%t] = getattr(self, 'h_lh_summlb_%s'%t).GetBinContent( getattr(self, 'h_lh_summlb_%s'%t).FindBin(   ret["sum_mlb"             ]) ) 
-                ret["lh_lepsdr_%s"%t] = getattr(self, 'h_lh_lepsdr_%s'%t).GetBinContent( getattr(self, 'h_lh_lepsdr_%s'%t).FindBin(   ret["lepsDR"              ]) ) 
-                ret["lh_met_%s"   %t] = getattr(self, 'h_lh_met_%s'%t   ).GetBinContent( getattr(self, 'h_lh_met_%s'%t   ).FindBin(   met                        ) ) 
-                ret["lh_zpt_%s"   %t] = getattr(self, 'h_lh_zpt_%s'%t   ).GetBinContent( getattr(self, 'h_lh_zpt_%s'%t   ).FindBin(   ret["lepsZPt"]             ) ) 
-                ret["lh_st_%s"    %t] = getattr(self, 'h_lh_st_%s'%t    ).GetBinContent( getattr(self, 'h_lh_st_%s'%t    ).FindBin(   ret['st']                  ) ) 
-                if not ret["lh_summlb_%s"%t]: ret["lh_summlb_%s"%t] = 1e-6
-                if not ret["lh_lepsdr_%s"%t]: ret["lh_lepsdr_%s"%t] = 1e-6
-                if not ret["lh_met_%s"   %t]: ret["lh_met_%s"   %t] = 1e-6
-                if not ret["lh_zpt_%s"   %t]: ret["lh_zpt_%s"   %t] = 1e-6
-                if not ret["lh_st_%s"    %t]: ret["lh_st_%s"    %t] = 1e-6
+                for u in ['_ana', '']:
+                    ret["lh%s_zpt_%s"%(u,t)] = getattr(self, 'h_lh%s_zpt_%s'%(u,t)).GetBinContent( getattr(self, 'h_lh%s_zpt_%s'%(u,t)).FindBin( ret["lepsZPt"]  ) ) 
+                    ret["lh%s_met_%s"%(u,t)] = getattr(self, 'h_lh%s_met_%s'%(u,t)).GetBinContent( getattr(self, 'h_lh%s_met_%s'%(u,t)).FindBin( met             ) ) 
+                    ret["lh%s_mlb_%s"%(u,t)] = getattr(self, 'h_lh%s_mlb_%s'%(u,t)).GetBinContent( getattr(self, 'h_lh%s_mlb_%s'%(u,t)).FindBin( ret["sum_mlb"]  ) ) 
+                    ret["lh%s_ldr_%s"%(u,t)] = getattr(self, 'h_lh%s_ldr_%s'%(u,t)).GetBinContent( getattr(self, 'h_lh%s_ldr_%s'%(u,t)).FindBin( ret["lepsDR" ]  ) ) 
+                    if not ret["lh%s_mlb_%s"%(u,t)]: ret["lh%s_mlb_%s"%(u,t)] = 1e-6
+                    if not ret["lh%s_ldr_%s"%(u,t)]: ret["lh%s_ldr_%s"%(u,t)] = 1e-6
+                    if not ret["lh%s_met_%s"%(u,t)]: ret["lh%s_met_%s"%(u,t)] = 1e-6
+                    if not ret["lh%s_zpt_%s"%(u,t)]: ret["lh%s_zpt_%s"%(u,t)] = 1e-6
+
+                    ret["cum%s_zpt_%s"%(u,t)] = getattr(self, 'h_lh%s_zpt_%s'%(u,t)).Integral( 1, getattr(self, 'h_lh%s_zpt_%s'%(u,t)).FindBin( ret["lepsZPt"]  ) ) 
+                    ret["cum%s_met_%s"%(u,t)] = getattr(self, 'h_lh%s_met_%s'%(u,t)).Integral( 1, getattr(self, 'h_lh%s_met_%s'%(u,t)).FindBin( met             ) ) 
+                    ret["cum%s_mlb_%s"%(u,t)] = getattr(self, 'h_lh%s_mlb_%s'%(u,t)).Integral( 1, getattr(self, 'h_lh%s_mlb_%s'%(u,t)).FindBin( ret["sum_mlb"]  ) ) 
+                    ret["cum%s_ldr_%s"%(u,t)] = getattr(self, 'h_lh%s_ldr_%s'%(u,t)).Integral( 1, getattr(self, 'h_lh%s_ldr_%s'%(u,t)).FindBin( ret["lepsDR" ]  ) ) 
 
         else:
             ret["srID"]      = -99
             for t in ['data', 'mc']:
-                ret["lh_summlb_%s"%t] = 0.
-                ret["lh_lepsdr_%s"%t] = 0.
-                ret["lh_met_%s"   %t] = 0.
-                ret["lh_zpt_%s"   %t] = 0.
-                ret["lh_st_%s"    %t] = 0.
+                for u in ['_ana', '']:
+                    ret["lh%s_mlb_%s"%(u,t)] = -999.
+                    ret["lh%s_ldr_%s"%(u,t)] = -999.
+                    ret["lh%s_met_%s"%(u,t)] = -999.
+                    ret["lh%s_zpt_%s"%(u,t)] = -999.
+
+                    ret["cum%s_mlb_%s"%(u,t)] = -999.
+                    ret["cum%s_ldr_%s"%(u,t)] = -999.
+                    ret["cum%s_met_%s"%(u,t)] = -999.
+                    ret["cum%s_zpt_%s"%(u,t)] = -999.
 
         
         fullret = {}
